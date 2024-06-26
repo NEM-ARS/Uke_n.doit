0a050c565bb34dbf1c96d484f3ada7168a293454
NEM-ARS <nemkovich2005@mail.ru>
2024-06-26 21:52:15 +0300

    bug fixes

--- script.js
+++ script.js
@@ -1,95 +1,75 @@
+//запуск анимации
+(() => document.querySelectorAll('.anim').forEach(item => item.classList.add('show')))()
 
-let anchors = document.querySelectorAll('.header__nav a[data-set]');
+//падение ноток при нажатии на пункты навигации
+document.querySelector('.header__nav').addEventListener('mousedown', e => {
+  if (e.target.dataset.note) {
+    createNote(e.target, e.target.dataset.note)
+  }
+})
 
-window.onscroll = function () {
+function createNote(e, mus) {
+  const sound = new Audio();
+  sound.src = `./sound/zvuk-notyi-${mus}.mp3`;
+  sound.currentTime = 0.0;
+  sound.play();
+
+  const note = document.createElement('img');
+  note.src = './icons/note.svg';
+  note.className = 'nav__anim'
+  note.style.left = e.getBoundingClientRect().left - 15 + e.clientWidth / 2 + 'px'
+  note.style.top = '10px'
+  document.body.prepend(note);
+
+  setTimeout(() => note.remove(), 1000);
+}
+
+//добавление класса к логотипу
+window.addEventListener('scroll', () => {
   let header__logo = document.querySelector('.header__logo');
-  scr()
+
   if (header__logo.getBoundingClientRect().top < 0) {
     header__logo.classList.add('header__logo-hover')
   } else {
     header__logo.classList.remove('header__logo-hover')
   }
-}
+})
 
-
-
-let Do = new Audio();
-
-const obj = {
-  do: './icons/note.svg',
-  re: './icons/note.svg',
-  mi: './icons/note.svg',
-}
-
-const au = {
-  do: './sound/zvuk-notyi-do.mp3',
-  re: './sound/zvuk-notyi-re.mp3',
-  mi: './sound/zvuk-notyi-mi.mp3',
-}
-for (let anchor of anchors) {
-  if (anchor) {
-    anchor.addEventListener('click', function (e) {
-      // e.preventDefault();
-      //   let anchorId = this.getAttribute('href');
-      let anchorD = this.getAttribute('data-set');
-      create(e.target, obj[anchorD], au[anchorD])
-
-
-    })
-  }
-}
-
-function create(e, src, mus) {
-  let img = document.createElement('img');
-  img.src = src;
-  Do.src = mus;
-  Do.currentTime = 0.0;
-
-  Do.play();
-  img.className = 'nav__anim'
-  img.style.left = e.getBoundingClientRect().left + 30 + 'px'
-  img.style.top = 0
-  // img.style.top = e.getBoundingClientRect().top + 'px'
-  document.body.append(img);
-  setTimeout(() => {
-    document.querySelector('.nav__anim').remove()
-  }, 900);
-}
-
-(() => document.querySelectorAll('.anim').forEach(item => item.classList.add('show')))()
-const bgTree = document.querySelector(".about");
-
-
-let cont = document.querySelector('.section-2');
-let box = document.querySelector('.section-2__wraper');
-let box1 = document.querySelector('.section-2__main');
-
+//горизонтальная прокрутка section-2
+window.addEventListener('scroll', () => scr())
 
 function scr() {
-  let top = -box.offsetTop
-  box1.style.transform = `translateX(${top}px)`
+  const wraper = -document.querySelector('.section-2__wraper').offsetTop;
+  const main = document.querySelector('.section-2__main');
+  main.style.transform = `translateX(${wraper}px)`
 }
 
-window.onresize = (e) => resize();
+window.onresize = () => resize();
 
 function resize() {
-  cont.style.height = 3630 + (window.innerHeight - window.innerWidth) + 'px'
+  document.querySelector('.section-2').style.height = 3630 + (window.innerHeight - window.innerWidth) + 'px'
 }
 resize()
 
-
+//смена стилей для блока toggle
 document.querySelector('.toggle').onclick = function (e) {
-  console.log(e.target.id)
-  const sum = document.querySelectorAll('.min45');
+  const price = document.querySelectorAll('.min45');
   const toggle__bg = document.querySelector('.toggle__bg');
-  if (e.target.id == 'in') {
-    sum.forEach(item => item.classList.add('min60'))
-    console.log(toggle__bg)
 
-    toggle__bg.style.transform = "translateX(100%)"
-  } else {
-    toggle__bg.style.transform = "translateX(0)"
+  toggle__bg.style.transform = e.target.id == 'min60' ? "translateX(100%)" : "translateX(0)"
+  price.forEach(item => item.style.transform = e.target.id == 'min60' ? "translateY(-26px)" : "translateY(0)")
+}
 
-    sum.forEach(item => item.classList.remove('min60'))
-  }
+
+//появление элементов списка program
+window.addEventListener('scroll', ()=>show())
+
+function show(){
+  document.querySelectorAll('.program__li').forEach(i=>{
+    if(i.getBoundingClientRect().top - 100 < window.innerHeight){
+      i.classList.add('show')
+    }else{
+      i.classList.remove('show')
+    }
+  })
 }
